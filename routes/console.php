e6beb4c89ec3ba1c0fd7e239cb91917d0f08c8dc
Jose Luis Salazar <lsalazar@gmx.de>
2024-10-27 12:22:21 -0600

    Add Schedule task to run the import proces daily

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('vinkos-pt:import-visitantes')
+    ->timezone('America/Mexico_City')
+    ->daily()
+    ->at('23:00');
